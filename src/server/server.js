2ce5fac0ded2c71092de1b79ffcf20dab123413b
Nick <ns285558@my.stchas.edu>
2026-02-28 17:41:47 -0600

    serve content from the client directory

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
+
 app.use(express.json());
+
+// serve client html
+app.use(express.static(path.join(__dirname, "../client")));
 
 app.post("/expenses", (req, res) => {
   console.log("Expense received:", req.body);
