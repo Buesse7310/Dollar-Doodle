1ad0b1960dfecebe0bf16183891a2d53a132be63
Nick <ns285558@my.stchas.edu>
2026-04-01 20:47:06 -0500

    fix casing

--- docs/phase2/database.sql
+++ docs/phase2/database.sql
@@ -195,7 +195,7 @@ CREATE TABLE Receipts (
     INDEX idx_receipt_uploaded (Rcpt_Uploaded_At)
 );
 
-INSERT INTO categories (Category_Name, Category_Icon, Budget_Percentage) VALUES
+INSERT INTO Categories (Category_Name, Category_Icon, Budget_Percentage) VALUES
 ('Food & Dining', '🍔', 0.00),
 ('Transportation', '🚗', 0.00),
 ('Shopping', '🛍️', 0.00),
